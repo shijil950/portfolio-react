e72fbf0c84ef1068020fd2d590c1c498a34cf7b1
shijilnk <shijilshaze38@gmail.com>
2026-06-21 18:24:01 +0530

    navbar error fixed completely

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg"> 
+    <nav className="navbar navbar-expand-lg">
       <div className="container-fluid d-flex justify-content-between align-items-center w-100">
+        
         <h2 className="logo">MUHAMMED SHIJIL NK</h2>
+        
         <button 
           className="navbar-toggler" 
           type="button" 
@@ -14,15 +16,12 @@ function Navbar() {
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
-          style={{ border: "1px solid white" }} 
+          style={{ border: "1px solid white" }}
         >
           <span className="navbar-toggler-icon" style={{ filter: "invert(1)" }}></span>
         </button>
 
-      
         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-          
-          
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/services">Services</Link></li>
@@ -31,8 +30,8 @@ function Navbar() {
             <li><Link to="/blog">Blog</Link></li>
             <li><Link to="/contact">Contact</Link></li>
           </ul>
-
         </div>
+
       </div>
     </nav>
   );
