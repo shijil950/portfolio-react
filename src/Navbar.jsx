19b40a8597513039fd7b2c1dba14805b68f7e2d7
shijilnk <shijilshaze38@gmail.com>
2026-06-21 18:15:08 +0530

    fixed navbar tags

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,17 +3,37 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <h2 className="logo">MUHAMMED SHIJIL NK</h2>
+    <nav className="navbar navbar-expand-lg"> 
+      <div className="container-fluid d-flex justify-content-between align-items-center w-100">
+        <h2 className="logo">MUHAMMED SHIJIL NK</h2>
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          style={{ border: "1px solid white" }} 
+        >
+          <span className="navbar-toggler-icon" style={{ filter: "invert(1)" }}></span>
+        </button>
 
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/portfolio">Portfolio</Link></li>
-        <li><Link to="/team">Team</Link></li>
-        <li><Link to="/blog">Blog</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
+      
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          
+          
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/services">Services</Link></li>
+            <li><Link to="/portfolio">Portfolio</Link></li>
+            <li><Link to="/team">Team</Link></li>
+            <li><Link to="/blog">Blog</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+
+        </div>
+      </div>
     </nav>
   );
 }
